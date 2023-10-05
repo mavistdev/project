02699dd2c48158491b161783950c1df5cf21d952
mavistdev <ogohyh22@gmail.com>
2023-10-05 16:57:45 +0300

    'я_ваш_гит_вертел'

--- employees.py
+++ employees.py
@@ -13,24 +13,32 @@ conn.execute('''CREATE TABLE IF NOT EXISTS employees
 
 conn.commit()
 
+#функция добавления сотрудников. На вход - имя, телефон, почта, зп
 def add_employee(name, phone, email, salary):
     conn.execute("INSERT INTO employees (name, phone, email, salary) VALUES (?, ?, ?, ?)",
-                 (name, phone, email, salary))
+                (name, phone, email, salary))
     conn.commit()
+    return 'Сотрудник успешно добавлен.'
 
+#функция обновления сотрудников. На вход - айди, имя, телефон, почта, зп
 def update_employee(id, name, phone, email, salary):
     conn.execute("UPDATE employees SET name = ?, phone = ?, email = ?, salary = ? WHERE id = ?",
-                 (name, phone, email, salary, id))
+                (name, phone, email, salary, id))
     conn.commit()
+    return "Сотрудник успешно изменен."
 
+#функция добавления сотрудников. На вход - айди
 def delete_employee(id):
     conn.execute("DELETE FROM employees WHERE id = ?", (id,))
     conn.commit()
+    return "Сотрудник успешно удален."
 
+#функция добавления сотрудников. На вход - ФИО
 def search_employee(name):
     cursor = conn.execute("SELECT * FROM employees WHERE name LIKE ?", (f'%{name}%',))
     return cursor.fetchall()
 
+#вывод меню
 def main_menu():
     print("1. Добавить сотрудника")
     print("2. Изменить сотрудника")
@@ -38,27 +46,26 @@ def main_menu():
     print("4. Поиск по ФИО")
     print("5. Выйти")
 
+#Меню добавления сотрудников
 def add_employee_menu():
     name = input("Введите ФИО: ")
     phone = input("Введите номер телефона: ")
     email = input("Введите адрес электронной почты: ")
     salary = float(input("Введите заработную плату: "))
-    add_employee(name, phone, email, salary)
-    print("Сотрудник успешно добавлен.")
+    print(add_employee(name, phone, email, salary))
 
+#
 def update_employee_menu():
     id = int(input("Введите ID сотрудника: "))
     name = input("Введите новое ФИО: ")
     phone = input("Введите новый номер телефона: ")
     email = input("Введите новый адрес электронной почты: ")
     salary = float(input("Введите новую заработную плату: "))
-    update_employee(id, name, phone, email, salary)
-    print("Сотрудник успешно изменен.")
+    print(update_employee(id, name, phone, email, salary))
 
 def delete_employee_menu():
     id = int(input("Введите ID сотрудника: "))
-    delete_employee(id)
-    print("Сотрудник успешно удален.")
+    print(delete_employee(id))
 
 def search_employee_menu():
     name = input("Введите ФИО сотрудника: ")
